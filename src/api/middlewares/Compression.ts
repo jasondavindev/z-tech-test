7431a25a2187f4cfa276862add1f85eaecf73d96
jasondavindev <jasondavin.dev@gmail.com>
2020-06-23 20:39:03 -0300

    add middlewares

---
+++ src/api/middlewares/Compression.ts
@@ -0,0 +1,10 @@
+import compression from 'compression';
+import { Request, Response, NextFunction } from 'express';
+import { ExpressMiddlewareInterface, Middleware } from 'routing-controllers';
+
+@Middleware({ type: 'before' })
+export default class CompressionMiddleware implements ExpressMiddlewareInterface {
+  public use(req: Request, res: Response, next: NextFunction): void {
+    compression()(req, res, next);
+  }
+}
